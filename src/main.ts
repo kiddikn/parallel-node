9a8dffcd07976b5737a479c165503fa3ee931a62
kiddikn <kiddikn@gmail.com>
2022-03-12 12:57:29 +0900

    receive output data

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,24 @@ import { Semaphore } from 'await-semaphore'
 const sleep = (msec: number) =>
   new Promise((resolve) => setTimeout(resolve, msec))
 
-const sleepCast = async (i: number, msec: number) => {
+interface Output {
+  num: number
+  at: string
+}
+
+const sleepCast = async (i: number, msec: number): Promise<Output> => {
   console.log(`call => ${i}`)
   const release = await semaphore.acquire()
   console.log(`acquire => ${i}`)
   await sleep(msec)
   console.log(new Date().toString() + ` => sleep ${msec}ms`)
   release()
+  return { num: i, at: new Date().toString() }
 }
 
 const semaphore = new Semaphore(3)
 const main = async () => {
-  await Promise.all([
+  const output: Output[] = await Promise.all([
     sleepCast(1, 300),
     sleepCast(2, 300),
     sleepCast(3, 600),
@@ -24,6 +30,7 @@ const main = async () => {
     sleepCast(7, 2000),
     sleepCast(8, 1000),
   ])
+  console.log(output)
 }
 
 main()
